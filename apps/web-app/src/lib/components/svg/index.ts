f7a376fdd5cfe73976c6259a99ad945b88b14d2e
Patrick Bacon-Blaber <pablaber225@gmail.com>
2024-12-24 10:55:58 -0500

    wip: room password almost working, need to join room on submit

--- apps/web-app/src/lib/components/svg/index.ts
+++ apps/web-app/src/lib/components/svg/index.ts
@@ -1,1 +1,3 @@
 export { default as Link } from './LinkSvg.svelte';
+export { default as Key } from './KeySvg.svelte';
+export { default as Alert } from './AlertSvg.svelte';
--- apps/web-app/src/routes/api/room/[roomId]/join/+server.ts
+++ apps/web-app/src/routes/api/room/[roomId]/join/+server.ts
@@ -3,11 +3,12 @@ import { z } from 'zod';
 import { db, schema } from '@squirrel/db';
 import { createCryptoUtils } from '$lib/utils';
 import { eq } from 'drizzle-orm';
+import { cookieUtils } from '$lib/server/utils/index.js';
 
 const { rooms } = schema;
 const cryptoUtils = createCryptoUtils(crypto.webcrypto.subtle as SubtleCrypto);
 
-export async function POST({ request, params }) {
+export async function POST({ request, params, cookies }) {
 	const { roomId } = params;
 
 	const bodySchema = z.object({
@@ -44,6 +45,7 @@ export async function POST({ request, params }) {
 		where: (rooms, { eq }) => eq(rooms.id, roomId)
 	});
 
+	// Verify the room exists
 	if (!existingRoom) {
 		return Response.json(
 			{
@@ -53,6 +55,21 @@ export async function POST({ request, params }) {
 		);
 	}
 
+	// If the room has a password, verify the password set in a cookie matches
+	if (existingRoom.password) {
+		const cookieMonster = cookieUtils.init(cookies);
+		const passwordValid = cookieMonster.testMasterPasswordCookie();
+		if (!passwordValid) {
+			return Response.json(
+				{
+					error: 'Invalid password'
+				},
+				{ status: 401 }
+			);
+		}
+	}
+
+	// If the room already has a guest, return an error
 	if (existingRoom.guestPublicKey) {
 		return Response.json(
 			{
@@ -62,6 +79,7 @@ export async function POST({ request, params }) {
 		);
 	}
 
+	// Update the room with the new guest and return the room
 	const [updatedRoom] = await db()
 		.update(rooms)
 		.set({
